997cc94d11935ff5bee6c78f9d0f2cc10c6cb681
nxsang <nxsang@axonactive.com>
2023-06-28 09:48:26 +0700

    add OnlineUser

---
+++ src/main/java/vn/edu/uit/chat_application/entity/OnlineUser.java
@@ -0,0 +1,36 @@
+package vn.edu.uit.chat_application.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "T_ONLINE_USER")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OnlineUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", nullable = false)
+    private UUID id;
+    @OneToOne
+    @JoinColumn(name = "user_id", unique = true, nullable = false)
+    private User user;
+    @Column(nullable = false)
+    private boolean online;
+}
