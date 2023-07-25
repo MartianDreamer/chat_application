a3c8a184b970a8b9a832d3854a55c0ca68865736
nxsang <nxsang@axonactive.com>
2023-07-25 17:29:02 +0700

    in-progress: refactor

--- src/main/java/vn/edu/uit/chat_application/controller/websocket/MessageController.java
+++ src/main/java/vn/edu/uit/chat_application/controller/websocket/MessageController.java
@@ -1,4 +1,4 @@
-package vn.edu.uit.chat_application.controller.rest.websocket;
+package vn.edu.uit.chat_application.controller.websocket;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
