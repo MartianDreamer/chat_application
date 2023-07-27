3b152b886ed069cb2017e229223f5ce062199b77
martiandreamer <nottrue@mail.com>
2023-07-27 12:09:55 +0700

    friend data

---
+++ tests/data/t_friend_relationship.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.t_friend_relationship (id, since, user_1_id, user_2_id) VALUES ('d7097c0b-19e2-40e3-821e-6f31ad8c9779', null, '715700ec-5064-4d04-9561-f2766d993bd7', 'e27804c3-42f6-4f0f-b02f-ab288d63f8eb');
+INSERT INTO public.t_friend_relationship (id, since, user_1_id, user_2_id) VALUES ('b0babdb3-8dd6-4091-8961-f91072c0a85f', null, '715700ec-5064-4d04-9561-f2766d993bd7', '60a5600b-6b5b-438f-8898-88aeba49c45f');
+INSERT INTO public.t_friend_relationship (id, since, user_1_id, user_2_id) VALUES ('7e7209eb-6376-40d6-bc87-5310b04a7f3d', null, '715700ec-5064-4d04-9561-f2766d993bd7', 'df8447db-f505-4017-96aa-4daf36c89654');
