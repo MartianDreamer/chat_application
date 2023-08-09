20e8df17debf8418f4c3fe4ec89c1de78ad6bb58
martiandreamer <nguyenxuansang9494@gmail.com>
2023-08-09 21:10:00 +0700

    fix code

--- tests/data/t_user.sql
+++ tests/data/t_user.sql
@@ -9,9 +9,9 @@ $$
                                            last_seen, online,
                                            password, phone_number, roles, username, valid_until)
                 VALUES (uuid_generate_v4(), false, true, null, '2023-07-28 14:20:46.421039',
-                        concat('martiandreamer', cnt, '@email.com'), null, false,
+                        concat('dummy', cnt, '@email.com'), null, false,
                         '$2a$12$3PW2mNHzbZnyeKmYrdkn/uUeCYs44WvCDd260NNJKkg4z/9Rc7kt6',
-                        concat('', 1000000000 + cnt), 'ROLE_USER', concat('martiandreamer', cnt), null);
+                        concat('', 1000000000 + cnt), 'ROLE_USER', concat('dummy', cnt), null);
             end loop;
     end;
 $$
